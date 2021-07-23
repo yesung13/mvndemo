8824beb368c889f5fefb35474cd0cd6e8a4939dd
yys-intellij <berno579@gmail.com>
2021-07-23 16:28:09 +0900

    ckeditor 적용

--- src/main/webapp/resources/ckeditor/config.js
+++ src/main/webapp/resources/ckeditor/config.js
@@ -3,8 +3,12 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    config.filebrowserUploadUrl = '/upload.do?type=Files',
+        config.filebrowserImageUploadUrl = '/upload.do?type=Images',
+        config.filebrowserUploadMethod = 'form'; //파일 오류났을때 alert띄워줌
 };
